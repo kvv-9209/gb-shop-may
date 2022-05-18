ca62558447ddd7e83a433cd44b0c1ee45927b04f
Artem Kropotov <aakropotov@alfabank.ru>
2022-05-18 20:10:40 +0300

    inital commit

---
+++ src/main/java/ru/gb/gbshopmay/GbShopMayApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gbshopmay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbShopMayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbShopMayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/gbshopmay/GbShopMayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gbshopmay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbShopMayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
