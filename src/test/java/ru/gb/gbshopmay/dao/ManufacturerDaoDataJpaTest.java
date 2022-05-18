46eef7e3d6498efbd66e3e7261654485afa141a3
Artem Kropotov <aakropotov@alfabank.ru>
2022-05-18 21:58:28 +0300

    lesson 2: Manufacturer DataJpaTest

---
+++ src/test/java/ru/gb/gbshopmay/dao/ManufacturerDaoDataJpaTest.java
@@ -0,0 +1,45 @@
+package ru.gb.gbshopmay.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import ru.gb.gbshopmay.config.ShopConfig;
+import ru.gb.gbshopmay.entity.Manufacturer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import(ShopConfig.class)
+class ManufacturerDaoDataJpaTest {
+
+    public static final String APPLE_COMPANY_NAME = "Apple";
+    public static final String MICROSOFT_COMPANY_NAME = "Microsoft";
+
+    @Autowired
+    ManufacturerDao manufacturerDao;
+
+    @Test
+    public void saveTest() {
+        Manufacturer manufacturer = Manufacturer.builder()
+                .name(APPLE_COMPANY_NAME)
+                .build();
+
+        Manufacturer savedManufacturer = manufacturerDao.save(manufacturer);
+
+        assertAll(
+                () -> assertEquals(1L, savedManufacturer.getId()),
+                () -> assertEquals(APPLE_COMPANY_NAME, savedManufacturer.getName()),
+                () -> assertEquals(0, savedManufacturer.getVersion()),
+                () -> assertEquals("User", savedManufacturer.getCreatedBy()),
+                () -> assertNotNull(savedManufacturer.getCreatedDate()),
+                () -> assertEquals("User", savedManufacturer.getLastModifiedBy()),
+                () -> assertNotNull(savedManufacturer.getLastModifiedDate())
+
+        );
+    }
+
+    // todo дз findAll, delete, update
+    // todo дз* попробовать через persistenceContext(либо через Autowoired) (через EntityManager)
+
+}
